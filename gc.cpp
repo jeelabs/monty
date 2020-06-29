f91043b85ca7df2e093642d55fd87a79f2c1a9d6
Jean-Claude Wippler <jc@wippler.nl>
2020-06-29 10:00:06 +0200

    using bit fields, probably not very useful

--- gc.cpp
+++ gc.cpp
@@ -1,8 +1,10 @@
-// Implementation of the main datatypes, i.e. Value, TypeObj, FrameObj, Context.
+// Memory allocation and garbage collection for objects and vectors.
 
 #define VERBOSE_GC      2 // show detailed memory allocation info & stats
 
 #include "monty.h"
+
+#include <assert.h>
 
 #if VERBOSE_GC // 0 = off, 1 = stats, 2 = detailed
 #if NATIVE
@@ -18,13 +20,6 @@
 
 #define PREFIX "\t\tgc "
 
-uint32_t totalAllocs,
-         totalBytes,
-         currAllocs,
-         currBytes,
-         maxAllocs,
-         maxBytes;
-
 #if NATIVE
 constexpr int MEM_BYTES = 24 * 1024;    // 24 Kb total memory
 constexpr int MEM_ALIGN = 16;           // 16-byte boundaries
@@ -33,8 +28,89 @@ constexpr int MEM_BYTES = 12 * 1024;    // 12 Kb total memory
 constexpr int MEM_ALIGN = 8;            // 8-byte boundaries
 #endif
 
-static uint32_t pool [MEM_BYTES / sizeof (uint32_t)]; // actual memory pool
-static uint32_t used [MEM_BYTES / MEM_ALIGN / 32];    // 1b per align boundary
+// 15-bit size header with 8-byte granularity can handle 256 KB memory
+struct Header {
+    uint16_t free :1;
+    uint16_t size :15; // in MEM_ALIGN units
+
+    void coalesce ();
+
+    size_t bytes () const { return size * MEM_ALIGN - sizeof (Header); }
+    Header& next () { return this[bytes() / sizeof (Header) + 1]; }
+};
+
+static Header mem [MEM_BYTES / sizeof (Header)];
+
+static size_t b2slots (size_t b) {
+    return (b + MEM_ALIGN);
+}
+
+static Header& header (void *p) {
+    assert(((uintptr_t) p) % MEM_ALIGN == 0);
+    assert(mem < p && p < mem + sizeof mem);
+    return ((Header*) p)[-1];
+}
+
+static void initMem () {
+    auto n = MEM_ALIGN / sizeof (Header);
+    mem[0].size = n;
+    mem[0].next().free = 1;
+    mem[0].next().size = MEM_BYTES / MEM_ALIGN - 2;
+    mem[0].next().next().size = 0;
+}
+
+void Header::coalesce () {
+    if (free == 1)
+        while (next().free)
+            size += next().size;
+}
+
+static void release (void* p) {
+    if (p != 0) {
+        auto& h = header(p);
+        assert(h.free == 0);
+        h.free = 1;
+    }
+}
+
+static void* allocate (size_t sz) {
+    if (mem[0].size == 0)
+        initMem();
+
+    for (auto h = &mem->next(); h < mem + sizeof mem; h = &h->next())
+        if (h->free) {
+            h->coalesce();
+            if (sz <= h->bytes()) {
+                // ...
+                h->free = 0;
+                return h + 1;
+            }
+        }
+
+    assert(false);
+    return 0; // ouch, ran out of memory
+}
+
+static void resize (void* p, size_t sz) {
+    // if sz == 0, use release instead
+    // if p == 0, use allocate() instead
+    auto& h = header(p);
+    // round sz
+    // if ==, done
+    // if <, release past end
+    // if > :
+    //   coalesce past end
+    //   if it now fits, done
+    //   if <, alloc new & move
+    //   if >, release past end
+}
+
+uint32_t totalAllocs,
+         totalBytes,
+         currAllocs,
+         currBytes,
+         maxAllocs,
+         maxBytes;
 
 void* Object::allocator (size_t sz, void* p) {
     if (sz != 0)
