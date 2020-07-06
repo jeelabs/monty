f2a7b740cca9f0ff10b0a03b42538a4f0b023638
Jean-Claude Wippler <jc@wippler.nl>
2020-07-06 10:13:18 +0200

    tweaks

--- lib/net/include/net.h
+++ lib/net/include/net.h
@@ -15,60 +15,59 @@ extern "C" {
 
 SpiHw< PinA<7>, PinA<6>, PinA<5>, PinA<4> > spi;
 
-void enchw_setup (enchw_device_t *dev) {
+void enchw_setup (enchw_device_t*) {
     spi.init();
 }
 
-void enchw_select (enchw_device_t *dev) {
+void enchw_select (enchw_device_t*) {
     spi.enable();
 }
 
-void enchw_unselect (enchw_device_t *dev) {
+void enchw_unselect (enchw_device_t*) {
     spi.disable();
 }
 
-uint8_t enchw_exchangebyte (enchw_device_t *dev, uint8_t byte) {
-    return spi.transfer(byte);
+uint8_t enchw_exchangebyte (enchw_device_t*, uint8_t b) {
+    return spi.transfer(b);
 }
 
 ip4_addr myip_addr = {0x02BCA8C0UL}; /* 192.168.188.2 */
 ip4_addr gw_addr = {0x01BCA8C0UL}; /* 192.168.188.1 */
 ip4_addr netmask = {0x000000FFUL}; /* 0.0.0.255 */
 
-static netif mchdrv_netif;
+static netif enc_if;
+static enc_device_t enc_hw;
 
-static enc_device_t mchdrv_hw;
+void mchdrv_poll (netif* netif) {
+    auto dev = (enc_device_t*)netif->state;
 
-void mchdrv_poll (netif *netif) {
-    auto encdevice = (enc_device_t*)netif->state;
-
-    if (enc_MII_read(encdevice, (enc_register_t) ENC_PHSTAT1) & (1 << 2))
+    if (enc_MII_read(dev, (enc_register_t) ENC_PHSTAT1) & (1 << 2))
         netif_set_link_up(netif);
     else
         netif_set_link_down(netif);
 
-    if (enc_RCR(encdevice, ENC_EPKTCNT)) {
-        pbuf *buf = NULL;
-        if (enc_read_received_pbuf(encdevice, &buf) == 0)
+    if (enc_RCR(dev, ENC_EPKTCNT)) {
+        pbuf* buf = NULL;
+        if (enc_read_received_pbuf(dev, &buf) == 0)
             netif->input(buf, netif);
     }
 }
 
-static err_t mchdrv_linkoutput (netif *netif, pbuf *p) {
-    enc_device_t *encdevice = (enc_device_t*)netif->state;
+static err_t mchdrv_linkoutput (netif* netif, pbuf* p) {
+    enc_device_t* dev = (enc_device_t*)netif->state;
 
-    enc_transmit_pbuf(encdevice, p);
+    enc_transmit_pbuf(dev, p);
     return ERR_OK;
 }
 
-err_t mchdrv_init (netif *netif) {
-    auto encdevice = (enc_device_t*)netif->state;
+err_t mchdrv_init (netif* netif) {
+    auto dev = (enc_device_t*)netif->state;
 
-    if (enc_setup_basic(encdevice) != 0 || enc_bist_manual(encdevice) != 0)
+    if (enc_setup_basic(dev) != 0 || enc_bist_manual(dev) != 0)
         return ERR_IF;
 
-    enc_ethernet_setup(encdevice, 4*1024, netif->hwaddr);
-    enc_set_multicast_reception(encdevice, 1); // always enable?
+    enc_ethernet_setup(dev, 4*1024, netif->hwaddr);
+    enc_set_multicast_reception(dev, 1); // always enable?
 
     netif->output = etharp_output;
     netif->linkoutput = mchdrv_linkoutput;
@@ -81,26 +80,26 @@ err_t mchdrv_init (netif *netif) {
 void mch_net_init () {
     lwip_init();
 
-    mchdrv_netif.hwaddr_len = 6; /* demo mac address */
-    mchdrv_netif.hwaddr[0] = 0;
-    mchdrv_netif.hwaddr[1] = 1;
-    mchdrv_netif.hwaddr[2] = 2;
-    mchdrv_netif.hwaddr[3] = 3;
-    mchdrv_netif.hwaddr[4] = 4;
-    mchdrv_netif.hwaddr[5] = 5;
+    enc_if.hwaddr_len = 6; /* demo mac address */
+    enc_if.hwaddr[0] = 0;
+    enc_if.hwaddr[1] = 1;
+    enc_if.hwaddr[2] = 2;
+    enc_if.hwaddr[3] = 3;
+    enc_if.hwaddr[4] = 4;
+    enc_if.hwaddr[5] = 5;
 
-    if (netif_add(&mchdrv_netif, &myip_addr, &netmask, &gw_addr, &mchdrv_hw,
+    if (netif_add(&enc_if, &myip_addr, &netmask, &gw_addr, &enc_hw,
                 mchdrv_init, ethernet_input) == NULL) {
         //LWIP_ASSERT("mch_net_init: netif_add (mchdrv_init) failed\n", 0);
         assert(false);
     }
 
-    netif_set_default(&mchdrv_netif);
-    netif_set_up(&mchdrv_netif);
+    netif_set_default(&enc_if);
+    netif_set_up(&enc_if);
 }
 
 void mch_net_poll () {
-    mchdrv_poll(&mchdrv_netif);
+    mchdrv_poll(&enc_if);
 }
 
 extern "C" uint32_t sys_now () {
