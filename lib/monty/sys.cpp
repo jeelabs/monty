8bd5aba6b2b679531b3215acad1881c2e52911cc
Jean-Claude Wippler <jc@wippler.nl>
2020-08-21 23:20:30 +0200

    add basic bound method test, ok except class.meth

--- lib/monty/sys.cpp
+++ lib/monty/sys.cpp
@@ -6,7 +6,7 @@
 using namespace Monty;
 
 //CG1 VERSION
-constexpr auto VERSION = "v0.93-30-g428da95";
+constexpr auto VERSION = "v0.93-31-gfef5853";
 
 static auto f_suspend (Vector const& vec, int argc, int args) -> Value {
     auto queue = &Interp::tasks;
