9d18be3e60ce87da980e80aab736d185447db734
Jean-Claude Wippler <jc@wippler.nl>
2020-08-25 19:46:06 +0200

    self-perpetuating git checkin mods ...

--- lib/monty/sys.cpp
+++ lib/monty/sys.cpp
@@ -6,7 +6,7 @@
 using namespace Monty;
 
 //CG1 VERSION
-constexpr auto VERSION = Q(167,"v0.93-81-g612a8ea");
+constexpr auto VERSION = Q(167,"v0.93-83-geb53c6d");
 
 static auto f_snooze (ArgVec const& args) -> Value {
     assert(2 <= args.num && args.num <= 4 && args[1].isInt());
