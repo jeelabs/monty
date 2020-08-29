adc796d2359caef81eb8f4b2d74d67ec8a2d5cbe
Jean-Claude Wippler <jc@wippler.nl>
2020-08-29 22:41:15 +0200

    oops, must call uart.read, to get the deadlines right

--- verify/d_test.py
+++ verify/d_test.py
@@ -23,26 +23,24 @@ def read(data, limit=-1, start=0):
         count = uart.read(data, limit, start, deadline)
     return count
 
-def write(data, limit=-1, start=0):
-    deadline = machine.ticks() + timeout
-    if limit < 0:
-        limit = len(data)
-    while start < limit:
-        start += uart.write(data, limit, start, deadline)
-    return start
-
-# to be placed elsewhere as i/o utilities, for use with any stream
-
 def readcount(data, count):
     deadline = machine.ticks() + timeout
     start = 0
     while start < count:
-        start += read(data, count, start, deadline)
+        start += uart.read(data, count, start, deadline)
     return count
 
 def readline(data, delim=10):
     deadline = machine.ticks() + timeout
     start = 0
     while start == 0 or data[start-1] != delim:
-        start += read(data, start+1, start, deadline)
+        start += uart.read(data, start+1, start, deadline)
     return start # include newline
+
+def write(data, limit=-1, start=0):
+    deadline = machine.ticks() + timeout
+    if limit < 0:
+        limit = len(data)
+    while start < limit:
+        start += uart.write(data, limit, start, deadline)
+    return limit
