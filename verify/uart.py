794ed0f0b29b50d28587524195b34c011b87493b
Jean-Claude Wippler <jc@wippler.nl>
2020-08-30 22:28:53 +0200

    cleanup

--- verify/uart.py
+++ verify/uart.py
@@ -5,20 +5,6 @@ n2 = uart.write(b'1234567abc\n', -1, 7, -1)
 n3 = uart.write(b'123defg\n456', 8, 3, -1)
 print(n1, n2, n3)
 
-if False:
-    import d_test as console
-    console.write(b'console write\n')
-
-    def print(*args):
-        sep = b''
-        for a in args:
-            console.write(sep)
-            console.write(a)
-            sep = b' '
-        console.write(b'\n')
-
-    print(b'abc', b'def')
-
 def cwrite(data):
     start = 0
     limit = len(data)
