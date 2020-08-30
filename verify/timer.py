e5593bb6801018cc8d56d3e28b81cc11ae01eb33
Jean-Claude Wippler <jc@wippler.nl>
2020-08-30 19:35:53 +0200

    tweak

--- verify/timer.py
+++ verify/timer.py
@@ -1,6 +1,6 @@
 async def loop():
     i = 0
-    while i < 10:
+    while i < 30:
         print(i, machine.ticks())
         i += 1
         sys.suspend(1)
--- verify/uart.py
+++ verify/uart.py
@@ -19,18 +19,15 @@ if False:
 
     print(b'abc', b'def')
 
-def cwrite(data, limit=-1, start=0):
-    deadline = machine.ticks() + 10000000
-    if limit < 0:
-        limit = len(data)
+def cwrite(data):
+    start = 0
+    limit = len(data)
     while start < limit:
         print('#',start)
-        #print()
         start += uart.write(data, limit, start, deadline)
     return limit
 
 for _ in range(10):
-    #console.write(b'12345678901234567890123456789012345678901234567890\n')
     cwrite(b'123456789-123456789+123456789~123456789=123456789\n')
 
 uart.close() # TODO
