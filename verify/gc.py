e177b1ca040901df252a6779fb2bb57d921acfa5
Jean-Claude Wippler <jc@wippler.nl>
2020-09-01 15:12:02 +0200

    put gc timing test inside a def

--- verify/gc.py
+++ verify/gc.py
@@ -4,13 +4,15 @@ a = []
 print(sys.gc_avail(), sys.gc_stats())
 print(sys.gc_avail(), sys.gc_stats())
 
-t0 = machine.ticks()
-for z in range(25):
-    for i in range(100):
-        l = [i,1,2,3,4,5,6,7,8,9]
-t1 = machine.ticks()
+def f():
+    t0 = machine.ticks()
+    for z in range(25):
+        for i in range(100):
+            l = [i,1,2,3,4,5,6,7,8,9]
+    t1 = machine.ticks()
+    print(t1-t0, 'ms')
 
-print(t1-t0, 'ms')
+f()
 print(sys.gc_avail(), sys.gc_stats())
 
 sys.gc_now()
