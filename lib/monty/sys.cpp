bd408d0cc35090214902041adcf416b5d0ba96d0
Jean-Claude Wippler <jc@wippler.nl>
2020-12-11 16:43:34 +0100

    merge to v0.96-1

--- lib/monty/sys.cpp
+++ lib/monty/sys.cpp
@@ -6,7 +6,7 @@
 using namespace Monty;
 
 //CG1 VERSION
-constexpr auto VERSION = Q(192,"v0.96");
+constexpr auto VERSION = Q(192,"v0.96-1");
 
 static auto f_suspend (ArgVec const& args) -> Value {
     assert(args.num == 1 && args[0].isInt());
