bfcc8f887d132671af34224df2ebf132250fe9db
Jean-Claude <jc@wippler.nl>
2021-01-19 22:58:55 +0100

    new stacklet demo, using Monty's code

--- demo/blinker/src/main.cpp
+++ demo/blinker/src/main.cpp
@@ -1,17 +1,10 @@
+#include "monty.h"
 #include <jee.h>
-#include <cstdint>
-#include <cstring>
 #include <setjmp.h>
 
-UartBufDev< PinA<2>, PinA<15> > console;
-
-int printf(const char* fmt, ...) {
-    va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
-    return 0;
-}
+using namespace monty;
 
 PinB<3> led;
-
 PinA<6> led1;
 PinA<5> led2;
 PinA<4> led3;
@@ -19,89 +12,57 @@ PinA<3> led4;
 PinA<1> led5;
 PinA<0> led6;
 
-void (*toggler[])() = {
-    led.toggle,
-    led1.toggle,
-    led2.toggle,
-    led3.toggle,
-    led4.toggle,
-    led5.toggle,
-    led6.toggle,
-};
-
-struct {
-    int stack [100];
-    jmp_buf top;
-    void (*task)(int);
-} stacks [7], *current;
-
-jmp_buf* caller;
-
-void suspend () {
-    int callee [1];
-    if (setjmp(current->top) == 0) {
-        // suspending: copy stack out and jump to caller
-        memcpy(current->stack, callee, (uintptr_t) caller - (uintptr_t) callee);
-        longjmp(*caller, 1);
-    } else {
-        // resuming: copy stack in and prevent re-resuming
-        memcpy(callee, current->stack, (uintptr_t) caller - (uintptr_t) callee);
-        memset(current->top, 0, sizeof current->top);
-#if 1
-        asm ("nop"); // FIXME without this, the code doesn't work (???)
-#endif
+void onOff (int n, bool f) {
+    switch (n) {
+        case 0: led.write(f); break;
+        case 1: led1.write(f); break;
+        case 2: led2.write(f); break;
+        case 3: led3.write(f); break;
+        case 4: led4.write(f); break;
+        case 5: led5.write(f); break;
+        case 6: led6.write(f); break;
     }
 }
 
-void resume (int num) {
-    current = &stacks[num];
-    jmp_buf bottom;
-    caller = &bottom;
-    if (setjmp(bottom) == 0) {
-        // either call for the first time, or resume saved state
-        if (current->task != nullptr)
-            current->task(num);
-        else
-            longjmp(current->top, 1);
-    }
-    // state has been saved, return to caller
-    current->task = nullptr;
-}
+// silly delay mechanism: keep putting ourselves (the current stacklet)
+// at the end of the ready queue until the time limit has been reached
 
 void delay_ms (uint16_t ms) {
     auto limit = ticks + ms;
-    while (ticks < limit)
-        suspend();
+    while (ticks < limit) {
+        ready.append(Stacklet::current);
+        Stacklet::suspend();
+    }
 }
 
-void toggleTask (int n) {
-    while (true) {
-        toggler[n]();
+struct Toggler : Stacklet {
+    int num;
+
+    Toggler (int n) : num (n) {}
+
+    auto run () -> bool override {
+        onOff(num, true);
         delay_ms(100);
-        toggler[n]();
-        delay_ms(100 * (n+1));
+        onOff(num, false);
+        delay_ms(100 * (num+1));
+        return true;
     }
-}
+};
 
-void demoPinToggle () {
-    // set up 7 independent blink tasks
-    for (int i = 0; i < 7; ++i)
-        stacks[i].task = toggleTask;
-
-    // run them for 100s
-    for (int i = 0; i < 100000; ++i) {
-        resume(i % 7);
-        asm("wfi");
-    }
-}
+char mem [10000];
 
 int main() {
-    console.init();
-    console.baud(115200, fullSpeedClock());
+    enableSysTick();
     led.mode(Pinmode::out);
     Port<'A'>::modeMap(0b1111011, Pinmode::out);
 
-    demoPinToggle();
+    setup(mem, sizeof mem);
 
-    while (true) {}
+    // create 7 stacklets, with different delays
+    for (int i = 0; i < 7; ++i)
+        new Toggler (i);
+
+    // minimal loop to keep stacklets going
+    while (ready.size() > 0)
+        ready.pull(0).asType<Stacklet>().resume();
 }
