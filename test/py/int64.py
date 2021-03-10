41a4ae148d3ad274721d2ede7dbe2358519fcb02
Jean-Claude <jc@wippler.nl>
2021-03-10 01:11:59 +0100

    add a few more missing int arith tests

---
+++ test/py/int64.py
@@ -0,0 +1,18 @@
+a = 1000000 * 2000000
+b = 3000000 * 4000000
+c = 5000000 * -6000000
+
+print(a, b, c)
+print(True, a<b)
+print(True, a>c)
+print(False, a==b)
+print(a|b, a&b, a^b)
+print(a<<2, a>>2)
+print(a*b, b*a, a/b, b/a, a%b, b%a)
+print(a+b, b+a, a-b, b-a)
+print(-a, -b, -c)
+print(~a, ~b, ~c)
+print(not a, not b, not c)
+print(bool(a), bool(b), bool(c))
+print(hash(a), hash(b), hash(c))
+print(abs(a), abs(-a), abs(c), abs(-c))
--- test/py/ints.py
+++ test/py/ints.py
@@ -18,3 +18,10 @@ print(10000000000000000)
 print(100000000000000000)
 print(1000000000000000000)
 print(-1000000000000000000)
+
+# add a few expressions to improve test coverage
+print(1&2, 1|2, 1^2, 1<<2, 1>>2, 1/2, 1%2)
+try:
+    a = 1 % 0
+except ZeroDivisionError as e:
+    print(e, e.trace())
